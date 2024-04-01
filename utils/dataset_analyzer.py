f87e65700814e877c04a76d2b771da3a8dce4970
Ruben Zissoldt <rzissoldt@gmail.com>
2024-04-01 14:37:24 +0200

    add

--- utils/dataset_analyzer.py
+++ utils/dataset_analyzer.py
@@ -17,8 +17,8 @@ class DatasetAnalyzer():
                 print('Hierarchy file path and HierarchyDicts file path is None. Not valid!')
                 return
             self.load_hierarchy_dicts(hierarchy_file_path=hierarchy_file_path, hierarchy_depth=hierarchy_depth)
-            hierarchy_dicts_file_path = os.path.join(self.path_to_results,self.dataset_name+'_'+str(self.image_count_threshold),'filtered_hierarchy_dicts.json')
-            os.makedirs(hierarchy_dicts_file_path, exist_ok=True)
+            hierarchy_dicts_path = os.path.join(self.path_to_results,self.dataset_name+'_'+str(self.image_count_threshold))
+            os.makedirs(os.path.join(hierarchy_dicts_file_path,'filtered_hierarchy_dicts.json'), exist_ok=True)
             with open(hierarchy_dicts_file_path, 'w') as outfile:
                 outfile.write(json.dump(self.filtered_hierarchy_dicts))
         else:
