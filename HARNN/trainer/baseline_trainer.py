f3696681afc1b8d2ad78495956e95212cbe25461
Ruben Zissoldt <rzissoldt@gmail.com>
2024-04-19 20:34:09 +0200

    add

--- HARNN/trainer/baseline_trainer.py
+++ HARNN/trainer/baseline_trainer.py
@@ -47,7 +47,7 @@ class BaselineTrainer():
         y = np.stack([image_tuple[1].numpy() for image_tuple in self.data_loader.dataset.image_label_tuple_list])
         for train_index, val_index in msss.split(X, y):
             train_dataset = torch.utils.data.Subset(self.data_loader.dataset, train_index)
-            val_dataset = torch.utils.data.Subset(self.data_loader.dataset, val_index)
+            val_dataset = torch.utils.data.Subset(copy.deepcopy(self.data_loader.dataset), val_index)
             val_dataset.dataset.is_training = False
             print('Train:',train_dataset.dataset.is_training)
             print('Val:',val_dataset.dataset.is_training)
