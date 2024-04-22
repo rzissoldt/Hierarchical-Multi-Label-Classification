47f238b9a905c7b5d1df8042bdecab05b8e7d01f
Ruben Zissoldt <rzissoldt@gmail.com>
2024-04-22 14:53:00 +0200

    added

--- utils/test_ddp.py
+++ utils/test_ddp.py
@@ -191,6 +191,10 @@ def demo_model_parallel(rank, world_size):
 
 
 if __name__ == "__main__":
+    # Get the path to the nvcc binary
+    nvcc_path = torch.utils.cpp_extension.get_cuda_path()
+
+    print("Path to nvcc binary:", nvcc_path)
     n_gpus = torch.cuda.device_count()
     assert n_gpus >= 2, f"Requires at least 2 GPUs to run, but got {n_gpus}"
     world_size = n_gpus
